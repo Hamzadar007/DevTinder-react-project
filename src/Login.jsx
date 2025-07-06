2bfc0719ff1471e288075787da71b1b441367d8b
hamzadar2013 <hamza.dar@camp1.tkxel.com>
2025-07-06 13:43:06 +0500

    login form UI

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {};
+
+  return (
+    <div className="flex justify-center items-center mt-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <div>
+            <fieldset className="fieldset  rounded-box w-xs border p-4 border-base-300">
+              <label className="label">Email ID</label>
+              <input
+                type="text"
+                className="input"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+
+              <label className="label">Password</label>
+              <input
+                type="password"
+                className="input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
